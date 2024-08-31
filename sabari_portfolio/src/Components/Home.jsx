fbb0e80f2c868f77a4d5ad67b59bed8cde72be74
velsabari <velsabari66@gmail.com>
2024-09-01 05:16:15 +0530

    completed balance work of what-i-do and 80% completed Resume page and responsive blance work for resume and initiate the footer page and design only completed routes pending

--- sabari_portfolio/src/Components/Home.jsx
+++ sabari_portfolio/src/Components/Home.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { FaDesktop } from "react-icons/fa6";
-import { FaPaintbrush } from "react-icons/fa6";
-import { FaFloppyDisk } from "react-icons/fa6";
+import { FaDesktop, FaPaintbrush, FaFloppyDisk, FaLink } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import Sidebar from "./sidebar";
 
@@ -177,79 +175,74 @@ const Home = () => {
       </section>
       <section id="what-i-do">
         <div className="bg-dark-2 flex flex-col min-h-screen ml-64">
-          <div className="relative w-full">
-            <p className="text-white font-poppins justify-center ml-80 mt-5 absolute translate-x-1/2 top-2 pt-14 font-semibold text-4xl">
+          <div className="relative w-full text-center">
+            <p className="text-white font-poppins mt-5 absolute inset-x-0 lg:top-3  right-0 mx-auto sm:bottom-3 pt-14 font-semibold text-3xl md:text-4xl">
               What I Do?
             </p>
-            <h1 className=" text-Gray-1  opacity-5 text-9xl   pt-10 font-poppins font-semibold flex items-center justify-center z-10">
+            <h1 className="text-Gray-1 opacity-5 text-6xl md:text-9xl pt-10 font-poppins font-semibold flex items-center justify-center z-10">
               SERVICE
             </h1>
           </div>
-          <div className="flex flex-wrap m-28">
-            <div className=" flex gap-7 w-full md:w-1/2 p-4 ">
-              <div>
-                <FaDesktop
-                  size={30}
-                  className="text-emerald-500 border-2 p-6 rounded bg-black border-black "
-                />
+          <div className="flex flex-wrap mx-6 md:mx- mb-10">
+            <div className="flex gap-5 md:gap-7 mt-7 w-full md:w-1/2 p-4">
+              <div className="relative bg-black h-fit rounded p-4">
+                <FaDesktop size={30} className="text-emerald-500" />
               </div>
-              <div>
-                <h3 className="text-white font-bold text-2xl">Web Design</h3>
-                <p className="text-gray-400 font-sans font-medium">
-                  Lisque persius interesset his et, in quot <br /> quidam persequeris
-                  vim, ad mea essent <br /> possim iriure.
+              <div className="max-w-sm">
+                <h3 className="text-white font-serif font-bold text-xl md:text-2xl">
+                  Web Design
+                </h3>
+                <p className="text-Gray-1 mt-1 font-sans font-medium leading-relaxed">
+                  Crafting visually engaging websites that are user-friendly and
+                  optimized for all devices. I focus on creating layouts that
+                  capture your brand’s essence while ensuring usability and
+                  performance.
                 </p>
               </div>
             </div>
-            <div className=" flex gap-7 w-full md:w-1/2 p-4">
-              <div>
-                <img
-                  src="https://img.icons8.com/ios-fillehttps://www.shutterstock.com/image-vector/hummer-icon-conception-spanner-1019382055?irclickid=QhjXO3zaOxyKR-DV90RWoWSaUkC1pi0nEzLV1U0&irgwc=1&pl=2052558-560528&utm_campaign=Icons8&utm_content=560528&utm_medium=Affiliate&utm_source=2052558&utm_term=d/50/000000/hammer-and-wrench.png"
-                  alt="Hammer and Wrench Icon"
-                  className="w-12 h-12"
-                />
+            <div className="flex gap-5 md:gap-7 mt-7  w-full md:w-1/2 p-4">
+              <div className="relative bg-black h-fit rounded p-4">
+                <FaLink size={30} className="text-emerald-500" />
               </div>
-              <div>
-                <h3 className="text-white font-bold text-2xl">
+              <div className="max-w-sm">
+                <h3 className="text-white font-serif font-bold text-xl md:text-2xl">
                   Backend Development
                 </h3>
-                <p className="text-gray-400 font-sans font-medium">
-                Lisque persius interesset his et, in quot <br /> quidam persequeris
-                vim, ad mea essent <br /> possim iriure.
+                <p className="text-Gray-1 mt-1 font-sans font-medium leading-relaxed">
+                  Building secure and scalable server-side solutions to power
+                  your applications. I develop robust APIs and handle data
+                  processing to ensure your app runs smoothly and efficiently.
                 </p>
               </div>
             </div>
-            <div className=" flex gap-7 w-full mt-5 md:w-1/2 p-4">
-              <div>
-                <FaPaintbrush
-                  size={30}
-                  className="text-emerald-500 border-2 p-6 rounded bg-black border-black"
-                />
+            <div className="flex gap-5 md:gap-7 mt-7 w-full md:w-1/2 p-4">
+              <div className="relative bg-black h-fit rounded p-4">
+                <FaPaintbrush size={30} className="text-emerald-500" />
               </div>
-              <div>
-                <h3 className="text-white font-bold text-2xl">
-                  App Design & Develop
+              <div className="max-w-sm">
+                <h3 className="text-white font-serif font-bold text-xl md:text-2xl">
+                  App Design & Development
                 </h3>
-                <p className="text-gray-400 font-sans font-medium">
-                Lisque persius interesset his et, in quot <br /> quidam persequeris
-                vim, ad mea essent <br /> possim iriure.
+                <p className="text-Gray-1 mt-1 font-sans font-medium leading-relaxed">
+                  Creating intuitive and responsive Web apps for a seamless user
+                  experience on any platform. My approach combines creative
+                  design with technical expertise to build Web apps that are
+                  both functional and visually appealing.
                 </p>
               </div>
             </div>
-            <div className=" flex gap-7 mt-5 w-full md:w-1/2 p-4">
-              <div>
-                <FaFloppyDisk
-                  size={30}
-                  className="text-emerald-500 border-2 p-6 rounded bg-black border-black"
-                />
+            <div className="flex gap-5 md:gap-7 mt-7 w-full md:w-1/2 p-4">
+              <div className="relative bg-black h-fit rounded p-4">
+                <FaFloppyDisk size={30} className="text-emerald-500" />
               </div>
-              <div>
-                <h3 className="text-white font-bold text-2xl">
+              <div className="max-w-sm">
+                <h3 className="text-white font-serif font-bold text-xl md:text-2xl">
                   Database Management
                 </h3>
-                <p className="text-gray-400  font-sans font-medium">
-                Lisque persius interesset his et, in quot <br /> quidam persequeris
-                vim, ad mea essent <br /> possim iriure.
+                <p className="text-Gray-1 mt-1 font-sans font-medium leading-relaxed">
+                  Efficiently managing and optimizing your data for reliability
+                  and performance. I design and implement robust database
+                  systems that ensure data integrity and security.
                 </p>
               </div>
             </div>
@@ -257,19 +250,144 @@ const Home = () => {
         </div>
       </section>
       <section id="resume">
-        <div className="bg-dark-1 flex flex-col min-h-screen ml-64">
-          <div className="relative w-full">
-            <p className="text-white font-poppins justify-center ml-96 mt-5 absolute translate-x-1/2 top-2 pt-14 font-semibold text-4xl">
+        <div className="bg-dark-1 flex flex-col min-h-[135vh]  md:ml-64">
+          <div className="relative w-full text-center">
+            <p className="text-white font-poppins   mt-5 absolute inset-x-0 right-0 mx-auto  top-2 pt-14 font-semibold text-4xl">
               Resume
             </p>
-            <h1 className=" text-Gray-1  opacity-5 text-9xl   pt-10 font-poppins font-semibold flex items-center justify-center z-10">
+            <h1 className="text-Gray-1 opacity-5 text-9xl pt-10 font-poppins font-semibold flex items-center justify-center z-10">
               SUMMARY
             </h1>
+          </div>
+
+          {/* Section Titles */}
+          <div className="flex flex-col mx-10 md:flex-row md:justify-between md:ml-10 ml-4 gap-4 mt-7">
+            <div className="flex flex-col">
+              <h1 className="text-white font-bold font-poppins text-2xl mb-4">
+                My Education
+              </h1>
+              <div className="grid grid-cols-1 gap-10">
+                <div className="bg-dark-2 relative w-full h-[245px] rounded shadow p-5">
+                  <button className="rounded w-fit text-white bg-emerald-400">
+                    2018-2022
+                  </button>
+                  <h2 className="text-white font-serif font-bold mt-4">
+                    B.E Mechanical Engineering
+                  </h2>
+                  <h3 className="text-emerald-500 font-sans font-semibold mt-1">
+                    Al Ameen Engineering College - Erode
+                  </h3>
+                  <h3 className="text-emerald-500 font-semibold mt-1">
+                    CGPA - 7.76
+                  </h3>
+                  <p className="text-Gray-1 mt-2 font-sans font-medium">
+                    <span className="text-emerald-300">"</span>My B.E. in
+                    Mechanical Engineering equipped me with practical skills in
+                    CAD modeling and project management, preparing me for a
+                    dynamic career in engineering.
+                    <span className="text-emerald-300">"</span>
+                  </p>
+                </div>
+
+                <div className="bg-dark-2 relative w-full h-[245px] rounded shadow p-5">
+                  <button className="rounded w-fit text-white bg-emerald-400">
+                    2024 - Present
+                  </button>
+                  <h2 className="text-white font-serif font-bold mt-4">
+                    MCA - Master of Computer Applications
+                  </h2>
+                  <h3 className="text-emerald-500 font-sans font-semibold mt-1">
+                    SRM University - Chennai
+                  </h3>
+                  <p className="text-Gray-1 mt-2 font-sans font-medium">
+                    <span className="text-emerald-300">"</span>I am currently
+                    pursuing a Master of Computer Applications (MCA) at SRM
+                    University, Chennai, where I am honing my skills in software
+                    development, database management, and advanced programming
+                    techniques.<span className="text-emerald-300">"</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col ml-4 mt-10 md:mt-0">
+              <h1 className="text-white font-bold font-poppins text-2xl mb-4">
+                My Experience & Skills
+              </h1>
+              <div className="grid grid-cols-1 gap-10">
+                <div className="bg-dark-2 relative w-full h-[245px] rounded shadow p-5">
+                  <button className="rounded w-fit text-white bg-emerald-400">
+                    2024 - Present
+                  </button>
+                  <h2 className="text-white font-serif font-bold mt-4">
+                    MERN Stack Developer - Intern
+                  </h2>
+                  <h3 className="text-emerald-500 font-sans font-semibold mt-1">
+                    Why Global Services - Chennai
+                  </h3>
+                  <p className="text-Gray-1 mt-2 font-sans font-medium">
+                    <span className="text-emerald-300">"</span>As a MERN Stack
+                    Developer Intern at Why Global Services in Chennai, I
+                    contributed to building and maintaining dynamic web
+                    applications, gaining hands-on experience in MongoDB,
+                    Express.js, React, and Node.js.
+                    <span className="text-emerald-300">"</span>
+                  </p>
+                </div>
+
+                <div className="bg-dark-2 relative w-full h-[245px] rounded shadow p-5">
+                  <button className="rounded w-fit text-white bg-emerald-400 font-semibold">
+                    My-Skills
+                  </button>
+                  <h2 className="text-white font-serif font-bold mt-4">
+                    Technical Skills
+                  </h2>
+                  <div className="mt-4 flex flex-wrap gap-4">
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;HTML5/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;CSS3/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Bootstrap/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Tailwind Css/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Javascript/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;React.js/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Node.js/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Express.js/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;MongoDB/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;UI/UX/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;Figma/&gt;
+                    </button>
+                    <button className="text-emerald-300 rounded bg-Gray-1">
+                      &lt;MongoDB/&gt;
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
       <section id="portfolio">
-        <div className="bg-dark-2 flex flex-col min-h-screen ml-64">
+        <div className="bg-dark-2 flex flex-col min-h-[150vh] ml-64">
           <div className="relative w-full">
             <p className="text-white font-poppins ml-96 mt-4 justify-center absolute translate-x-1/2 top-2 pt-14 font-semibold text-4xl">
               My Work
@@ -292,6 +410,18 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <footer>
+        <div className="bg-dark-2 h-[180px] flex  items-center ml-64">
+          <h2 className="text-gray-400 font-sans font-semibold ml-14 ">
+            Copyright © 2024{" "}
+            <span className="text-emerald-500">Sabari Sakthivel</span>. All
+            Rights Reserved.
+          </h2>
+          <h2 className="text-gray-400 font-sans font-semibold ml-80">
+            Terms & Policy <span className=" ml-10">Disclaimer</span>
+          </h2>
+        </div>
+      </footer>
     </>
   );
 };
